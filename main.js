9b397b661fff97bca0823844fb2276ab335e34f6
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2021-07-22 14:38:28 -0700

    updated name with underscore

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 
-document.getElementById('getSchools').addEventListener('click', getSchools);
+// document.getElementById('getSchools').addEventListener('click', getSchools);
+document.getElementById('getAPI').addEventListener('click', getAPI);
 
 function getSchools() {
     fetch('schools.json')
         .then((res) => res.json())
         .then((data) => {
             console.log(data)
+        })
+}
+
+function getAPI() {
+    fetch('https://trc-residential-api.herokuapp.com/res-schools')
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data)
+            let output = '<h2>Residential Schools</h2>';
+            data.forEach(function (res_schools) {
+                output += `
+                <ul>
+                    <li>Name: ${res_schools.name}</li>
+                    <li>Name: ${res_schools.city}</li>
+                </ul>
+                
+                
+                `
+
+            })
         })
 }
